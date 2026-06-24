a968712dfc38b97c0aba7fd54384e43caeda0605
DEschweiler <dennis.eschweiler@rwth-aachen.de>
2026-06-24 13:51:42 +0200

    added sister-project tile layout

--- static/js/index.js
+++ static/js/index.js
@@ -18,41 +18,19 @@ $(document).ready(function() {
 	
     bulmaSlider.attach();
 
-    // Toggle main downstream accordion
-    $('.downstream-main-toggle').click(function() {
-        var accordion = $(this).closest('.downstream-accordion');
-        var content = accordion.find('.downstream-main-content');
-        
-        accordion.toggleClass('is-active');
-        content.slideToggle(300);
-        
-        // If main is closed, close all nested items to keep it clean for next time
-        if (!accordion.hasClass('is-active')) {
-            $('.nested-accordion-item').removeClass('is-active');
-            $('.nested-accordion-content').slideUp(0);
-            $('.bibtex-content-box').slideUp(0);
-        }
-    });
+    // Expand / collapse the full description of a project card
+    $('.read-more-btn').click(function() {
+        var btn = $(this);
+        var card = btn.closest('.project-card');
+        var expanded = card.toggleClass('is-expanded').hasClass('is-expanded');
 
-    // Toggle nested accordion items (mutually exclusive)
-    $('.nested-accordion-header').click(function() {
-        var item = $(this).closest('.nested-accordion-item');
-        var content = item.find('.nested-accordion-content');
-        var siblingItems = item.siblings('.nested-accordion-item');
-        
-        // Close siblings
-        siblingItems.removeClass('is-active');
-        siblingItems.find('.nested-accordion-content').slideUp(300);
-        siblingItems.find('.bibtex-content-box').slideUp(300);
-        
-        // Toggle current
-        item.toggleClass('is-active');
-        content.slideToggle(300);
+        btn.toggleClass('is-expanded');
+        btn.find('.read-more-label').text(expanded ? 'Read less' : 'Read more');
     });
 
     // Toggle BibTeX inline views
     $('.bibtex-toggle-btn').click(function() {
-        var bibtexBox = $(this).closest('.nested-project-details').find('.bibtex-content-box');
+        var bibtexBox = $(this).closest('.project-card').find('.bibtex-content-box');
         bibtexBox.slideToggle(200);
     });
 
